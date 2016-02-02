085104127f4f74da906c9c874d51f4466409fb72
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-02-02 10:32:14 +0700

    ENHANCEMENT: Enable continuous integration with code coverage. Coverage now 99%. Fixed clearWhiteList method

--- code/WhitelistGenerator.php
+++ code/WhitelistGenerator.php
@@ -1,17 +1,17 @@
 <?php
 class WhitelistGenerator extends Object implements Flushable {
 
-	static function generateWhitelist(){
+	public static function generateWhitelist(){
 		$whitelist = self::generateWhitelistRules();
 		self::syncCacheFilesystem($whitelist);
 	}
-	
-	static function generateWhitelistRules(){
+
+	public static function generateWhitelistRules(){
 		//get all URL rules
 		$rules = Config::inst()->get('Director', 'rules');
-		
+
 		$allTopLevelRules = array();
-		foreach($rules as $pattern => $controllerOptions) {
+		foreach ($rules as $pattern => $controllerOptions) {
 			//match first portion of the URL, either delimited by slash or colon or end-of-line
 			if (preg_match('/^(.*?)(\/|:|$)/', $pattern, $matches)){
 				if (!empty($matches[1])){
@@ -19,36 +19,36 @@ class WhitelistGenerator extends Object implements Flushable {
 				}
 			}
 		}
-		
+
 		$filteredRules = array();
-		foreach($allTopLevelRules as $rule) {
+		foreach ($allTopLevelRules as $rule) {
 			if (strpos($rule, '$') !== false) {
 				if ($rule === '$Controller') {
 					//special case for Controllers, add all possible controllers
 					$subControllers = ClassInfo::subclassesFor(new Controller());
-					
-					foreach($subControllers as $controller){
+
+					foreach ($subControllers as $controller){
 						array_push($filteredRules, $controller);    //add the controller name as a link
-					} 
-					
+					}
+
 				} elseif ($rule === '$URLSegment') {
 					//special case for SiteTree, add all possible top Level Pages
 					$topLevelPages = SiteTree::get()->filter('ParentID', 0);
-					
-					foreach($topLevelPages as $page) {
+
+					foreach ($topLevelPages as $page) {
 						$link = $page->RelativeLink();
 						array_push($filteredRules, trim($link, '\/ ')); //remove trailing or leading slashes from links
 					}
-					
+
 				} else {
 					user_error('Unknown wildcard URL match found: '.$rule, E_WARNING);
-					
+
 				}
 			} else {
 				//add the rule to a new list of rules
 				array_push($filteredRules, $rule);
 			}
-			
+
 		}
 
 		//filter duplicates (order doesn't matter here, as we are only interested in the first level of the rules)
@@ -56,21 +56,22 @@ class WhitelistGenerator extends Object implements Flushable {
 		return $filteredRules;
 	}
 
-	static protected function array_delete($array, $element) {
-		return array_diff($array, [$element]);
+	protected static function array_delete($array, $element) {
+        $elementArray = array($element);
+		return array_diff($array, $elementArray);
 	}
 
 	/**
 	 * Sync the list of all top-level routes with the file system whitelist cache
 	 */
-	static protected function syncCacheFilesystem($whitelist) {
+	protected static function syncCacheFilesystem($whitelist) {
 		$dir = BASE_PATH . DIRECTORY_SEPARATOR . Config::inst()->get('WhitelistGenerator', 'dir');
-		
+
 		$whitelistFolderContents = scandir($dir);
-		
+
 		//create list of files to create
 		$toCreate = array();
-		foreach($whitelist as $listItem){
+		foreach ($whitelist as $listItem){
 			if (!in_array($listItem, $whitelistFolderContents)) {
 				if (!empty($listItem)) {    //don't include empty files, such as the file for /
 					array_push($toCreate, $listItem);
@@ -80,8 +81,8 @@ class WhitelistGenerator extends Object implements Flushable {
 
 		//create list of files to delete
 		$toDelete = array();
-		foreach($whitelistFolderContents as $file){
-			if (!in_array($file, array('','..','.','.htaccess'))) {    //exclude things that should stay in the folder 
+		foreach ($whitelistFolderContents as $file){
+			if (!in_array($file, array('','..','.','.htaccess'))) {    //exclude things that should stay in the folder
 				if (!in_array($file, $whitelist)) {
 					array_push($toDelete, $file);
 				}
@@ -89,29 +90,29 @@ class WhitelistGenerator extends Object implements Flushable {
 		}
 
 		//delete files which are no longer necessary
-		foreach($toDelete as $delete) {
+		foreach ($toDelete as $delete) {
 			unlink($dir . DIRECTORY_SEPARATOR . $delete);
 		}
 
 		//create new whitelist items as files
-		foreach($toCreate as $create) {
+		foreach ($toCreate as $create) {
 			touch($dir . DIRECTORY_SEPARATOR . $create);
 		}
 	}
-	
-	static function ensureWhitelistFolderExists(){
+
+	public static function ensureWhitelistFolderExists(){
 		$dir = BASE_PATH . DIRECTORY_SEPARATOR . Config::inst()->get('WhitelistGenerator', 'dir');
 		if (!file_exists($dir)) {
 			mkdir($dir); //create a new whitelist dir
 			chmod($dir,0777);    //make sure it is readable by the web-server user
-			//create a htaccess file to ensure that the whitelist cache directory is not web-accessible 
+			//create a htaccess file to ensure that the whitelist cache directory is not web-accessible
 			file_put_contents($dir.DIRECTORY_SEPARATOR.'.htaccess', "Deny from all\n");
 		}
 	}
-	
+
 	public static function clearWhitelist(){
 		$dir = BASE_PATH . DIRECTORY_SEPARATOR . Config::inst()->get('WhitelistGenerator', 'dir');
-		if (!file_exists($dir)) {
+		if (file_exists($dir)) {
 			array_map('unlink', glob($dir."/*"));
 		}
 	}
